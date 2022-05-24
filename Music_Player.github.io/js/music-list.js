d0a358ca0fa8867217743288d77f5b6c75a9a191
Sanjay Aba Agalave <71866134+Mr-Sanjay@users.noreply.github.com>
2022-05-24 23:28:45 +0530

    Add files via upload

---
+++ Music_Player.github.io/js/music-list.js
@@ -0,0 +1,80 @@
+let allMusic =[
+    {
+        name : "Alan_Walker_-_Force",
+        artist: "Alan Walker",
+        img: "music01",
+        src: "song01"
+    },
+    {
+        name : "Alan_Walker_-_Alone",
+        artist: "Alan_Walker",
+        img: "music02",
+        src: "song02"
+    },
+    {
+        name : "Alan_Walker_-_The_Spectre",
+        artist: "Alan_Walker",
+        img: "music03",
+        src: "song03"
+    },
+    {
+        name : "Alan_Walker_Ft__lselin",
+        artist: "Alan_Walker",
+        img: "music04",
+        src: "song04"
+    },
+    {
+        name : "CKay_-_Love_Nwantiti_ft",
+        artist: "Joeboy & Kuami",
+        img: "music05",
+        src: "song05"
+    },
+    {
+        name : "Alan_Walker_Sabrina_ft",
+        artist: "Alan_Walker",
+        img: "music06",
+        src: "song06"
+    },
+    {
+        name : "Travor_Daniel_-_Falling",
+        artist: "Travor_Daniel",
+        img: "music07",
+        src: "song07"
+    },
+    {
+        name : "_ishq_mubarak_full_song_",
+        artist: "Arijit_Singh",
+        img: "music08",
+        src: "song08"
+    },
+    {
+        name : "Dil_mang_rha_hai_mohiat",
+        artist: "DK_Creation",
+        img: "music09",
+        src: "song09"
+    },
+    {
+        name : "Itna_Pyaar_Karo_Song_Lyrics",
+        artist: "Rishi_k,Emraan_H",
+        img: "music10",
+        src: "song10"
+    },
+    {
+        name : "Tari_Nazar_Ne_Ye_Kya_Kar_Kiya",
+        artist: "Unknown artist",
+        img: "music11",
+        src: "song11"
+    },
+    {
+        name : "Meri_Zindagi_Hai_Tu_lyrics",
+        artist: "Unknown artist",
+        img: "music12",
+        src: "song12"
+    },
+    {
+        name : "Tumse_Bhi_Zyada_Song",
+        artist: "Ahan_Shetty, Tara_Sutaria",
+        img: "music13",
+        src: "song13"
+    }
+]
